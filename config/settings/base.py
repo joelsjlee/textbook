294e490ae0acf91f435ebbcf68d4533892b99d2c
Scott Enderle <scott.enderle@gmail.com>
2019-07-31 18:46:34 +0000

    fix css mimetype

--- config/settings/base.py
+++ config/settings/base.py
@@ -3,6 +3,9 @@ Base settings to build other settings files upon.
 """
 
 import environ
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 
 ROOT_DIR = (
     environ.Path(__file__) - 3
