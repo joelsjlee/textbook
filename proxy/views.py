a9ed33e1d8c4744d7791dcd76d7052b5f0534a2c
Vicente Guallpa <vincentguallpa@gmail.com>
2019-08-14 15:08:40 -0400

    refractoring of views.py

--- proxy/views.py
+++ proxy/views.py
@@ -9,70 +9,50 @@ from django.core.management import call_command
 import os.path
 import zipfile
 
-# Create your views here.
+
+##############################################################
+# VIEWS
+##############################################################
+# View for textbooks and other non-Django files
 @login_required
 def proxy(request, static_path):
+    # Prevent unauthorized users from accessing other pages
     if not (request.user.is_staff or request.user.is_student):
         return redirect("/")
-    # print('static path: ' + static_path)
     template_path = 'proxy/' + static_path
-    # print('template_path: ' + template_path)
-    # print(os.listdir('proxy/templates/proxy'))
-    if os.path.isfile('proxy/templates/' + template_path):
-        if template_path.endswith(".html"):
-            html_page = loader.render_to_string(template_path)
-            header, body = get_header_body(html_page)
-            header = edit_header_styling(header)
-            html_page = header + body
-            template = Template(html_page)
-            context = Context()
-            try:
-                return HttpResponse(template.render(context))
-            except ValueError:
-                call_command('collectstatic', verbosity=0, interactive=False)
-                return HttpResponse(template.render(context))
-        else:
-            return render(request, template_path)
-    else:
+    if not os.path.isfile('proxy/templates/' + template_path):
         raise Http404("no static site matches the given query.")
+    # Handle paths to textbooks
+    if template_path.endswith(".html"):
+        template = get_template(template_path)
+        context = Context()
+        try:
+            return HttpResponse(template.render(context))
+        except ValueError:
+            call_command('collectstatic', verbosity=0, interactive=False)
+            return HttpResponse(template.render(context))
+    else:
+        return render(request, template_path)
 
 
-# view for home
+# View for home page
 def home(request):
-    path = "pages/home.html"
-    return render(request, path, {'text_info_list': get_text_info_list()})
+    template_name = "pages/home.html"
+    text_info_list = get_text_info_list("../app/proxy/templates/proxy/texts")
+    return render(request, template_name, {"text_info_list": text_info_list})
 
 
-# view for upload
+# View for file upload
 @login_required
 def file_upload(request):
     if not request.user.is_staff:
         return redirect("/")
     if request.method == "POST":
-        ok_to_process = True
         textbooks = request.FILES.getlist("textbook_file")
         article_zips = request.FILES.getlist("article_file")
         keywords = request.POST.getlist("keyword")
-        # check if form has at least one file or keyword
-        if (not textbooks) and (not article_zips) and (not keywords[0]):
-            messages.error(request, "At least one file must be uploaded")
-            ok_to_process = False
-        # check for faulty textbook files
-        err = err_check_textbooks(textbooks)
-        if err:
-            err = "Failed to upload files, the following textbooks have incorrect file types: " + err
-            messages.error(request, err)
-            messages.info(request, "Only textbooks of .txt type are accepted")
-            ok_to_process = False
-        # check for faulty article zip file and article files
-        err = err_check_articles(article_zips)
-        if err:
-            err = "Faile to upload files, the following articles have incorrect file types: " + err
-            messages.error(request, err)
-            messages.info(request, "Article zipfile must contain only articles of type .txt")
-            ok_to_process = False
-        # process everything is it is all ok
-        if (ok_to_process):
+        # Process files if it is all error free
+        if not error_check_files(request, textbooks, article_zips, keywords):
             # process textbooks
             success = process_textbooks(textbooks)
             if success:
@@ -91,6 +71,91 @@ def file_upload(request):
     return render(request, "pages/file_upload.html")
 
 
+# Check files for errors, returns True if they are any
+def error_check_files(request, textbooks, article_zips, keywords):
+    # Check for blank form
+    if (not textbooks) and (not article_zips) and (not keywords[0]):
+        messages.error(request, "At least one file must be uploaded")
+        return True
+    # Check for errors in textbook files
+    err = err_check_textbooks(textbooks)
+    if err:
+        messages.error(request, err)
+        messages.info(request, "Only textbooks of .txt type are accepted")
+        return True
+    # Check for errors in articles
+    err = err_check_articles(article_zips)
+    if err:
+        messages.error(request, err)
+        messages.info(request, "Article zipfile must contain only articles of type .txt")
+        return True
+    return False
+
+
+##############################################################
+# HELPER FUNCTIONS FOR TEXTBOOK VIEW
+##############################################################
+# Generate template for Django context
+def get_template(template_path):
+    html_page = loader.render_to_string(template_path)
+    head, body = get_head_body(html_page)
+    head = edit_header_styling(head)
+    html_page = head + body
+    return Template(html_page)
+
+
+# Get head and body of string HTML page
+def get_head_body(html_page):
+    head_ending_index = html_page.find("</head>")
+    head = html_page[:head_ending_index]
+    body = html_page[head_ending_index:]
+    return head, body
+
+
+# Edit the styling of header
+def edit_header_styling(header):
+    new_header = ""
+    lines = header.split("\n")
+    for line in lines:
+        if "<head>" in line:
+            line += "\n  {% load static %}"
+        if "<meta property=\"og:image\" content=\"/assets/open-graph-logo.png\"/>" in line:
+            line = "    <meta property=\"og:image\" content=\"{% static \'open-graph-logo.png\' %}\"/>"
+        if "<link rel=\"stylesheet\" href=\"/assets/css/style.css\"/>" in line:
+            line = "  <link rel=\"stylesheet\" type=\"text/css\" href=\"{% static \'css/style.css\' %}\"/>"
+        if "<link rel=\"apple-touch-icon-precomposed\" sizes=\"180x180\" href=\"/assets/apple-touch-icon-precomposed.png\"/>" in line:
+            line = "  <link rel=\"apple-touch-icon-precomposed\" sizes=\"180x180\" href=\"{% static \'apple-touch-icon-precomposed.png\' %}\"/>"
+        if "<link rel=\"shortcut icon\" href=\"/assets/favicon.ico\"/>" in line:
+            line = "  <link rel=\"shortcut icon\" href=\"{% static \'favicon.ico\' %}\"/>"
+        new_header += line + "\n"
+    return new_header
+
+
+##############################################################
+# HELPER FUNCTIONS FOR HOME VIEW
+##############################################################
+# Get the textbook info for display
+def get_text_info_list(textbook_dir):
+    text_info_list = []
+    if os.path.isdir(textbook_dir):
+        for text_filename in os.listdir(textbook_dir):
+            text_info = []  # holds [{raw file name}, {formatted file name}]
+            text_info.append(text_filename)
+            text_info.append(format_text_name(text_filename))
+            text_info_list.append(text_info)
+    return text_info_list
+
+
+# Format the text filename
+def format_text_name(text_filename):
+    text_filename = text_filename.replace("_", " ")
+    text_filename = text_filename.replace("-", " ")
+    return titlecase(text_filename)
+
+
+##############################################################
+# HELPER FUNCTIONS FOR FILE UPLOAD VIEW
+##############################################################
 # cheack for faulty textbook files
 def err_check_textbooks(textbooks):
     err = ""
@@ -99,6 +164,7 @@ def err_check_textbooks(textbooks):
             err += textbook.name + ", "
     if err:
         err = err[:-2]
+        err = "Failed to upload files, the following textbooks have incorrect file types: " + err
     return err
 
 
@@ -116,6 +182,7 @@ def err_check_articles(article_zips):
                     err += article_name + ", "
     if err:
         err = err[:-2]
+        err = "Failed to upload files, the following articles have incorrect file types: " + err
     return err
 
 
@@ -173,52 +240,3 @@ def process_keywords(keywords):
     if keywords_processed:
         keywords_processed = keywords_processed[:-2]
     return keywords_processed
-
-
-# Format the text file name
-def format_text_name(text_name):
-    text_name = text_name.replace("_", " ")
-    text_name = text_name.replace("-", " ")
-    return titlecase(text_name)
-
-
-# Get the text information for display
-def get_text_info_list():
-    text_info_list = []
-    try:
-        text_dir_names = os.listdir("../app/proxy/templates/proxy/texts")
-        for text_dir_name in text_dir_names:
-            text_info = []  # holds [{raw file name}, {formatted file name}]
-            text_info.append(text_dir_name)
-            text_info.append(format_text_name(text_dir_name))
-            text_info_list.append(text_info)
-    except:
-        pass
-    return text_info_list
-
-
-# Get header and body of string HTML page
-def get_header_body(html_page):
-    head_end_tag_index = html_page.find("</head>")
-    head_section = html_page[:head_end_tag_index]
-    body_section = html_page[head_end_tag_index:]
-    return head_section, body_section
-
-
-# Edit the styling of header
-def edit_header_styling(header):
-    new_header = ""
-    lines = header.split("\n")
-    for line in lines:
-        if "<head>" in line:
-            line += "\n  {% load static %}"
-        if "<meta property=\"og:image\" content=\"/assets/open-graph-logo.png\"/>" in line:
-            line = "    <meta property=\"og:image\" content=\"{% static \'open-graph-logo.png\' %}\"/>"
-        if "<link rel=\"stylesheet\" href=\"/assets/css/style.css\"/>" in line:
-            line = "  <link rel=\"stylesheet\" type=\"text/css\" href=\"{% static \'css/style.css\' %}\"/>"
-        if "<link rel=\"apple-touch-icon-precomposed\" sizes=\"180x180\" href=\"/assets/apple-touch-icon-precomposed.png\"/>" in line:
-            line = "  <link rel=\"apple-touch-icon-precomposed\" sizes=\"180x180\" href=\"{% static \'apple-touch-icon-precomposed.png\' %}\"/>"
-        if "<link rel=\"shortcut icon\" href=\"/assets/favicon.ico\"/>" in line:
-            line = "  <link rel=\"shortcut icon\" href=\"{% static \'favicon.ico\' %}\"/>"
-        new_header += line + "\n"
-    return new_header
